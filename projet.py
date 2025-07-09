2951bf84cd2aa7db09f323327f968a969ca3e2fb
moufidamenif <menifmoufi4@gmail.com>
2025-07-09 18:34:11 +0100

    modification projet.py

--- projet.py
+++ projet.py
@@ -1,4 +1,7 @@
 Data=[1,3,5]
-print("le produit est ",Prod(Data))
-print("le min est ",min(Data))
-print("le max est ",max(Data))
+if Data : 
+	print("le produit est ",Prod(Data))
+	print("le min est ",min(Data))
+	print("le max est ",max(Data))
+else : 
+	print("dossier vide ")
